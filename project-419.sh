f4ce8bc98cca1f1e6db30d860b951a554ba1e8cf
281677160 <66651997+281677160@users.noreply.github.com>
2020-08-31 19:51:39 +0800

    Update project-419.sh

--- project-419.sh
+++ project-419.sh
@@ -4,6 +4,7 @@
 # 说明：
 # 除了第一行的#!/bin/bash不要动，其他的设置，前面带#表示不起作用，不带的表示起作用了（根据你自己需要打开或者关闭）
 #
+#源码里面几乎囊括了所有插件，所以基本不需要添加插件了
 
 # 修改openwrt登陆地址,把下面的192.168.2.2修改成你想要的就可以了
 sed -i 's/192.168.1.1/192.168.2.2/g' ./package/base-files/files/bin/config_generate
@@ -17,7 +18,7 @@ sed -i 's/OpenWrt/OpenWrt-123/g' ./package/base-files/files/bin/config_generate
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' ./package/lean/default-settings/files/zzz-default-settings
 
 
-# 修改内核版本
+# 修改内核版本（版本内核默认4.19，还有4.14跟4.9内核）
 #sed -i 's/KERNEL_PATCHVER:=4.19/KERNEL_PATCHVER:=4.9/g' ./target/linux/x86/Makefile
 
 
